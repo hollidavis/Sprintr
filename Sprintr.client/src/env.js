a01faaf94c6bd254ecfdfc6466963fc70303a09b
hollidavis <hollidavis19@gmail.com>
2021-08-09 14:10:46 -0600

    env & mongo done

--- Sprintr.client/src/env.js
+++ Sprintr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'http://localhost:3000/' : ''
+export const domain = 'holli-codeworks.us.auth0.com'
+export const clientId = 'SkX9v30AyWYKkIsCNZKmn45U6qLbq0gr'
+export const audience = 'http://sprintr.com'
