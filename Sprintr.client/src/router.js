997a6616a1999d5ede004893c0f902c3218ce910
hollidavis <hollidavis19@gmail.com>
2021-08-10 15:06:54 -0600

    created client files 🗃

--- Sprintr.client/src/router.js
+++ Sprintr.client/src/router.js
@@ -10,17 +10,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: loadPage('HomePage')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: loadPage('AboutPage')
-  },
-  {
-    path: '/account',
-    name: 'Account',
-    component: loadPage('AccountPage'),
-    beforeEnter: authGuard
   }
 ]
 
