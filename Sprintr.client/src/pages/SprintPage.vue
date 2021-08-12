f65b0f5d08df776890bbc37ecb6e9680bfebd4cc
hollidavis <hollidavis19@gmail.com>
2021-08-12 10:52:35 -0600

    sprint load page fixed

--- Sprintr.client/src/pages/SprintPage.vue
+++ Sprintr.client/src/pages/SprintPage.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <Sprint />
+      <Sprint :sprint="activeSprint" />
     </div>
   </div>
 </template>
 
 <script>
+import { computed, watchEffect } from '@vue/runtime-core'
+import { sprintsService } from '../services/SprintsService'
+import Pop from '../utils/Notifier'
+import { useRoute } from 'vue-router'
+import { AppState } from '../AppState'
 export default {
   name: 'SprintPage',
   setup() {
-    return {}
+    const route = useRoute()
+    watchEffect(async() => {
+      try {
+        if (!route.params.sprintId) {
+          return
+        }
+        await sprintsService.setActiveSprint(route.params.sprintId)
+      } catch (error) {
+        Pop.toast(error, 'error')
+      }
+    })
+    return {
+      activeSprint: computed(() => AppState.activeSprint)
+    }
   }
 }
 </script>
